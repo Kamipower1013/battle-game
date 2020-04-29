649dc16e16082d3c61636ab5e14f71f6f8070600
Kamipower1013 <46644678+Kamipower1013@users.noreply.github.com>
2020-04-29 23:26:40 +0800

    Create PosPoint.cs
    A Class about position

---
+++ PosPoint.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpg
+{
+    class Pos
+    {
+        public int x;
+        public int y;
+
+        public Pos(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+
+
+
+
+}
